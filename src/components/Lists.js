aab20a341668804c6c2a4a578f3f862b1c7a4e97
fremin33 <florian.dev33@gmail.com>
2018-07-21 01:12:29 +0200

    get Bordeaux bar

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -9,23 +9,35 @@ const request = require('request');
 
 
 class Lists extends Component {
+  constructor () {
+    super();
+    this.state = {
+      bars: [],
+    }
+  }
 componentDidMount() {
-  request({
-    url: 'https://api.foursquare.com/v2/venues/search',
-    method: 'GET',
-    qs: {
-      client_id: process.env.REACT_APP_CLIENT_ID,
-      client_secret: process.env.REACT_APP_CLIENT_SECRET,
-      ll: ' 44.8404400,-0.5805000',
-      categoryId: '4bf58dd8d48988d116941735',
-      v: '20180323',
-    }
-  }, function(err, res, body) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(body);
-    }
+  var url = new URL("https://api.foursquare.com/v2/venues/search"),
+      params = {
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        client_secret: process.env.REACT_APP_CLIENT_SECRET,
+        ll: ' 44.8404400,-0.5805000',
+        categoryId: '4bf58dd8d48988d116941735',
+        v: '20180323'
+      }
+
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  fetch(url).then((results) => {
+   return results.json();
+  }).then((data) => {
+    let bars = data.response.venues.map((bar) => {
+      return(
+        <div>
+            <p>{bar.name}</p>
+          </div>
+      )
+    })
+    this.setState({bars: bars});
+    // console.log(this.state);
   });
 }
   
